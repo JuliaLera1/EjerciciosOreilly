ca80cc3ba2c5007a5374c227ac9052d5a3f75b97
Julia Lera <julia.lera@bosonit.com>
2023-10-04 13:50:20 +0200

    csv subido a ejSanfran

--- src/main/scala/org/example/ejSanFran.scala
+++ src/main/scala/org/example/ejSanFran.scala
@@ -1,5 +1,20 @@
 package org.example
-
+import org.apache.spark.sql.SparkSession
 object ejSanFran {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder().master("local[1]")
+      .appName("Fire")
+      .getOrCreate()
+
+    val sampleDF = spark
+      .read
+      .option("samplingRatio", 0.001)
+      .option("header", true)
+      .csv("C:/users/julia.lera/downloads/Fire_incidents.csv")
+    sampleDF.show()
+
+
+  }
 
 }
