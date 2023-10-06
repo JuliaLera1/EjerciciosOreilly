fe8741325d580039953bb9c8ac31d82a948dcd7c
Julia Lera <julia.lera@bosonit.com>
2023-10-06 13:53:10 +0200

    algunas funciones agg

--- src/main/scala/org/example/ejSanFran.scala
+++ src/main/scala/org/example/ejSanFran.scala
@@ -69,13 +69,13 @@ object ejSanFran {
 //importante poner en la segunda parte de to_timestamp en qué formato está nuestra fecha, MM es mes, mm es minuto
     //fireTsDF.select("IncidentDate", "ArrivalTS", "CloseTS", "AlarmTS").show(5,false)
 
-    //val resultado = fireTsDF.withColumn("Delay", col("ArrivalTS") - col("IncidentDate"))
+//    val resultado = fireTsDF.withColumn("Delay", col("ArrivalTS") - col("IncidentDate"))
     //val resultado = fireTsDF.withColumn("Delay", datediff(col("ArrivalTS"), col("IncidentDate"))) //esto me da 0 siempre, no cuenta las horas creo
     val resultado = fireTsDF.withColumn(
       "Delay",
       (unix_timestamp(col("ArrivalTS")) - unix_timestamp(col("IncidentDate"))) / 3600)
     //esta forma me da las horas
-    //resultado.show()
+    resultado.select("neighborhood_district", "Delay").groupBy("neighborhood_district").agg(avg("Delay").as("AvgDelay")).orderBy(col("AvgDelay").desc).show()
 
 
   }
