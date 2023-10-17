6adb746a88168aac00372ac56235d58e306d2b83
Julia Lera <julia.lera@bosonit.com>
2023-10-17 09:25:57 +0200

    End-to-End ejercicios

--- src/main/scala/org/example/IotDevices.scala
+++ src/main/scala/org/example/IotDevices.scala
@@ -32,9 +32,23 @@ object IotDevices {
     */
     //cómo uso map? por qué no puedo hacer como en el libro d=>d.temp>25
 //EJERCICIOS EXTRA END_TO_END
-    //IoT.where(col("battery_level").lt(2)).show()
+    /*
+    IoT.where(col("battery_level").lt(2)).show()
     val ds=IoT.select("cn").where(col("c02_level").gt(1500))
+    println("Estos son los países que alguna vez superan el nivel 1500 de CO2: \n")
     ds.groupBy("cn").count().show()
+    val MinMax = IoT.agg(max(col("temp")).alias("max_temp"), min("temp").alias("min_temp"),
+      max("battery_level").alias("max_battery"), min("battery_level").alias("min_battery"),
+      max("c02_level").alias("max_c02"), min("c02_level").alias("min_c02"),
+      max("humidity").alias("max_humidity"), min("humidity").alias("min_humidity"))
+    println("Estos son los valores máximos y mínimos de ciertos campos: ")
+    MinMax.show()
+    val minMaxRows = IoT.join(MinMax, Seq())
+    //De esta forma podemos obtener qué entradas dan con esos valores máximos y mínimos
+    minMaxRows.filter(col("temp")===col("max_temp")).show()
+    */
+    IoT.groupBy("cn").agg(avg("temp").alias("temp_avg")).orderBy("temp_avg").show()
+
 
 
   }
