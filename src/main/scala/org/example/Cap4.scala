7d372b8b1577160f98ca4fd7a78ed4858f638739
Julia Lera <julia.lera@bosonit.com>
2023-10-17 14:21:52 +0200

    Empezando cap4: creación df, primeras consultas

--- src/main/scala/org/example/Cap4.scala
+++ src/main/scala/org/example/Cap4.scala
@@ -1,5 +1,58 @@
 package org.example
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
+import scala.language.postfixOps
 object Cap4 {
+  def main(args: Array[String]) {
+
+    //empezamos la sesión de Spark
+    val spark: SparkSession = SparkSession.builder().master("local[1]")
+      .appName("Cap4")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val csvFile="src/main/resources/departuredelays.csv"
+    val sch = "date STRING, delay INT, distance INT, origin STRING, destination STRING"
+
+    // Read and create a temporary view
+    // Infer schema (note that for larger files you may want to specify the schema)
+    val df = spark.read.format("csv")
+      //.option("inferSchema", "true")
+      .schema("date STRING, delay INT, distance INT, origin STRING, destination STRING")
+      .option("header", "true")
+      .load(csvFile)
+
+    // es el mismo df
+//    val df3 = spark.read
+//      .option("inferSchema", "true")
+//      .option("header", "true")
+//      .csv("src/main/resources/departuredelays.csv")
+
+    // Create a temporary view
+    df.createOrReplaceTempView("us_delay_flights_tbl")
+    //PRIMEROS EJERCICIOS
+    /*
+    df.select("distance", "origin", "destination").where(col("distance").gt(1000)).orderBy(desc("distance")).show()
+    val df1=df.select("*").where(col("origin")==="SFO" && col("destination")==="ORD")
+    df1.where(col("delay").gt(120)).orderBy(desc("delay")).show()
+
+     */
+/*
+* It seems there were many significantly delayed flights between these two cities, on dif‐
+ferent dates. (As an exercise, convert the date column into a readable format and find
+the days or months when these delays were most common. Were the delays related to
+winter months or holidays?)
+*/
+    val dfDates= df.withColumn("dateTS", to_timestamp(col("date"), "MMddHHmm")).drop("date")
+     //pone el año 1970, pero no es muy importante para los ejercicios
+     val dfDates1 = dfDates.select("*").where(col("origin") === "SFO" && col("destination") === "ORD")
+    dfDates1.where(col("delay").gt(120)).groupBy(month(col("dateTS"))).count().show()
+    dfDates1.where(col("delay").gt(120)).groupBy(dayofmonth(col("dateTS"))).count().orderBy(desc("count")).show()
+
+
+
+  }
 
 }
