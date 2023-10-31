90d1c0a93b2ae8969b597604ace133e1c0fa762a
Julia Lera <julia.lera@bosonit.com>
2023-10-31 14:06:49 +0100

    empezar cap6

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -3,6 +3,8 @@ package org.example
 import org.apache.hadoop.shaded.com.google.common.io.MoreFiles
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.hive.HiveSessionStateBuilder
+//import org.apache.hadoop.hive.conf.HiveConf
 
 
 
@@ -22,7 +24,9 @@ val conf: SparkConf = new SparkConf().setAppName("name").setMaster("local")
 
     implicit val spark: SparkSession = SparkSession.builder().config(sc.getConf).master("local[1]")
       .appName("SparkByExamples.com")
-      .enableHiveSupport()
+      //.enableHiveSupport()
+//      .config("spark.sql.warehouse.dir", "/user/hive/warehouse") // Set Hive warehouse directory
+//      .config("hive.metastore.uris", "thrift://your-metastore-uri:9083") // Set Hive Metastore URI
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
@@ -36,10 +40,11 @@ val conf: SparkConf = new SparkConf().setAppName("name").setMaster("local")
     //ejSanFran.Fire(spark)
     //MnM.basico(spark)
     //IotDevices.arcjson(spark)
-    MoreFunctions.execute()
+    //MoreFunctions.execute() //no funciona!!
     //ExArray.ex1()
     //ExArray.avanzado()
-    //airports.airpots()
+    airports.airpots()
+   // bloggers.ejblog()
 
 
   }
--- src/main/scala/org/example/bloggers.scala
+++ src/main/scala/org/example/bloggers.scala
@@ -1,5 +1,22 @@
 package org.example
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
 object bloggers {
+  def ejblog()(implicit spark: SparkSession): Unit={
+    import spark.implicits._
+
+    case class Bloggers(id: IntegerType, first: StringType, last: StringType, url: StringType, date: StringType,
+                        hits: IntegerType, campaigns: Array[StringType])
+    val bloggers = "src/main/resources/blogs.json"
+//    val bloggersDS = spark.read
+//      .format("json")
+//      .option("path", bloggers)
+//      .load()
+//      .as[Bloggers]
+
+
+  }
 
 }
