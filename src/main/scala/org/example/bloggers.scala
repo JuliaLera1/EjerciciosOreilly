06f7f10c05c2040d8d2e903bcd471d489592e603
Julia Lera <julia.lera@bosonit.com>
2023-11-02 14:13:59 +0100

    bloggers Dataset creado

--- src/main/scala/org/example/bloggers.scala
+++ src/main/scala/org/example/bloggers.scala
@@ -2,19 +2,23 @@ package org.example
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Encoder, Encoders}
 
 object bloggers {
+  case class Bloggers(id: BigInt, first: String, last: String, url: String,
+                      hits: BigInt, campaigns: Array[String])
+  //definir el case class fuera de la función, necesita tenerlo a nivel global
   def ejblog()(implicit spark: SparkSession): Unit={
-    import spark.implicits._
 
-    case class Bloggers(id: IntegerType, first: StringType, last: StringType, url: StringType, date: StringType,
-                        hits: IntegerType, campaigns: Array[StringType])
+    val enc = org.apache.spark.sql.Encoders.product[Bloggers]
+
     val bloggers = "src/main/resources/blogs.json"
-//    val bloggersDS = spark.read
-//      .format("json")
-//      .option("path", bloggers)
-//      .load()
-//      .as[Bloggers]
+    val bloggersDS = spark.read
+      .format("json")
+      .option("path", bloggers)
+      .load()
+      .as(enc)
+    bloggersDS.printSchema()
 
 
   }
